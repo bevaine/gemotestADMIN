7e03289c4bfcd9a1ea3cce25481443f29399fd2f
Evgeny Dymchenko <bevaine@gmail.com>
2017-10-13 14:40:44 +0300

    fix

--- common/config/main.php
+++ common/config/main.php
@@ -3,6 +3,25 @@ return [
     'language' => 'ru',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=localhost;port=5432;dbname=work',
+            'username' => 'admin',
+            'password' => 'itrTest',
+            'charset' => 'utf8',
+        ],
+        'GemoTestDB' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'sqlsrv:Server=sw-sky-cl;Database=OrdersFromCACHE',
+            'username' => 'importfromcache',
+            'password' => 'import',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            'useFileTransport' => true,
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
