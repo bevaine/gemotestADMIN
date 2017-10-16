373812a6311a3dc43251e6e190256abca9af929e
Evgeny Dymchenko <bevaine@gmail.com>
2017-10-16 17:28:31 +0300

    fix

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -13,7 +13,7 @@ $assets = UsersAsset::register($this);
 
 <header class="main-header">
 
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg">Гемотест</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
 
     <nav class="navbar navbar-static-top" role="navigation">
 
