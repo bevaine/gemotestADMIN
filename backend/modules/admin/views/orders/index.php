2bce61d43bf25ebb7af95c71fa62381cb9d4e46b
Evgeny Dymchenko <bevaine@gmail.com>
2018-01-10 17:08:45 +0300

    fix

--- backend/modules/admin/views/orders/index.php
+++ backend/modules/admin/views/orders/index.php
@@ -13,11 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="orders-to-export-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p>
-        <?= Html::a('Create Orders To Export', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
